038877bc2df8f11ae0ba5184f37d282c8ac55b5d
Amos <asidelnik@gmail.com>
2018-04-30 07:33:02 +0300

    set up 2

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ app.listen(8000);
 
 app.use(express.static('public'));
 app.use(express.static(__dirname + '/node_modules'));
+//app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 
 app.get('/', function(request, response){
     response.send({ name: 'John', age: 30 });
-  });
+});
